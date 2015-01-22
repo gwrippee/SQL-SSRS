4af23b02e20eb25d43806436c99ea62b2b691d1e
James Tuttle <jamest@tasupply.com>
2015-01-22 14:31:54 -0800

    update

--- Finance/uspCreditMemoCreditChargesKathyMiller.sql
+++ Finance/uspCreditMemoCreditChargesKathyMiller.sql
@@ -12,8 +12,50 @@
 
 
 SELECT *
-FROM OPENQUERY(GSFL2K,'
-	SELECT * 
-	FROM file
-	WHERE
+FROM OPENQUERY(tsGSFL2K,'
+
+	SELECT ohcust		AS Sold_To
+		  ,cmname
+		  ,ohcm
+		  ,ohref#
+		  ,olitem
+		 /* ,ohpric */
+		  ,ohemds		AS Sub_Total
+		  ,ohsam1		AS Misc_Amount_Delv_Chg
+		  ,ohsam2		AS Misc_Amount_Restock
+		  ,ohsam3		AS Misc_Amount_Discount
+		  ,ohsam4		AS Misc_Amount_Frt_Hndl
+		  ,ohsam5		AS Misc_Amount_Fuel
+
+		  ,ohspc1		AS Misc_Ext_Delv_Chg
+		  ,ohspc2		AS Misc_Ext_Restock
+		  ,ohspc3		AS Misc_Ext_Discount
+		  ,ohspc4  		AS Misc_Ext_Frt_Hndl
+		  ,ohspc5		AS Misc_Ext_Fuel
+
+		  ,oleprc		AS Line_Ext_Price
+		  ,olesp1		AS Line_Ext_Cut_Chg
+		  ,olesp2
+		  ,olesp3
+		  ,olesp4
+		  ,olesp5
+
+		  ,''''		AS rebate
+
+		  ,''''				AS tl_loads
+		  ,''''			AS trip
+
+		
+
+	FROM oohead oh
+	LEFT JOIN ooline ol ON ( oh.ohco = ol.olco
+		    AND oh.ohloc = ol.olloc
+		    AND oh.ohord# = ol.olord#
+		    AND oh.ohrel# = ol.olrel#
+		    AND oh.ohcust = ol.olcust )
+	LEFT JOIN custmast cm ON cm.cmcust = oh.ohcust
+
+	WHERE oh.ohord# = 190486
 ')
+
+
