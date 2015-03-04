af395b23db864d9d2f9570da901652e67064736a
James Tuttle <jamest@tasupply.com>
2015-03-04 12:30:02 -0800

    Changed to add PARMS

--- Marketing/Pacmat/uspArmstrongDisplaysByCustomer.sql
+++ Marketing/Pacmat/uspArmstrongDisplaysByCustomer.sql
@@ -12,30 +12,46 @@
 **	the SQL below.									**
 **													**
 ******************************************************/
+-- UPDATE by James Tuttle 03/04/2015				--
+--													--
+--    Need to add customer's Armstrong ID.  That is --
+-- in field=vncvcust in the table vendcust where the-- 
+-- vncvend=16037									--
+--=====================================================
 
 ALTER PROC uspArmstrongDisplaysByCustomer
 	@BillCodes	varchar(2)
 AS
-SELECT *
-FROM OPENQUERY (GSFL2K,'
-	SELECT     cbcust					AS CustNum
-			   ,cmname					AS CustName
-			   ,SUBSTRING(cmadr3,0,24)	AS City
-			   ,SUBSTRING(cmadr3,24,2)	AS State
-			   ,smname					AS Salesman			
-			   ,cbblcd					AS BillCD
-			   ,bcdesc					AS BillCDDesc
+BEGIN
+	DECLARE @sql varchar(MAX) 
+	SET @sql = 'SELECT *
+	FROM OPENQUERY (GSFL2K,''
+		SELECT     cbcust					AS CustNum
+				   ,cmname					AS CustName
+				   ,SUBSTRING(cmadr3,0,24)	AS City
+				   ,SUBSTRING(cmadr3,24,2)	AS State
+				   ,smname					AS Salesman			
+				   ,cbblcd					AS BillCD
+				   ,bcdesc					AS BillCDDesc
+				   ,vncvcust				AS Armstrong_ID
 
-	FROM custbill
-		  LEFT JOIN blcdmast ON cbblcd = bcblcd
-		  LEFT JOIN custmast ON cbcust = cmcust
-		  LEFT JOIN salesman ON smno = cmslmn
+		FROM custbill cb
+			  LEFT JOIN blcdmast bcm ON cb.cbblcd = bcm.bcblcd
+			  LEFT JOIN custmast cm ON cbcust = cm.cmcust
+			  LEFT JOIN salesman sm ON sm.smno = cm.cmslmn
+			  LEFT JOIN vendcust vc ON vc.vnccust = cm.cmcust
 
-	/* WHERE cbblcd = ''  + @BillCodes  +''	*/
-
-	ORDER BY smname ASC
-			,cbcust ASC
-')
+		 WHERE cbblcd =  ' + '''' + '''' + @BillCodes + '''' + ''''	+ '
 
 
---	uspBillingCodes 'AU'
+
+		ORDER BY smname 
+				,cbcust  
+	'')'
+
+	EXEC (@sql)
+END
+
+--	uspArmstrongDisplaysByCustomer 'AJ'
+-- AU, AH, A0, lc
+--> 32 codes based on the 16037 vendor <--
