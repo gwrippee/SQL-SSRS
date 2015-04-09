f1fe02f94f30302e2c9251f8bfdb29832e09434b
James Tuttle <jamest@tasupply.com>
2015-04-09 08:25:23 -0700

    Added for Purchasing

---
+++ Purchasing/CallReportingPurchasing.sql
@@ -0,0 +1,82 @@
+/* 
+
+Created By:  Thomas Van Nuys
+Date Created: 8/19/2014
+
+SR #24493
+
+Run this after the TA CDR report data is loaded to SQL 
+within the Call Center reporting process
+
+Open a past version of the Excel Purchasing call report and
+replace the Data with the results of the query below
+
+ 
+File from Fonality Report:  CDR reports, options inbound, outbound and InterOffice
+	CDR_Report.csv \\storage01\Shared\Departmental Data\Sales Marketing\Customer Service\Fonality Call Logs
+	
+File from Fonality Report: ACD report:  Completed Calls :: Detail  
+	acd_report.csv \\storage01\Shared\Departmental Data\Sales Marketing\Customer Service\Fonality Call Logs
+When a new agent needs to be tracked, edit the CallLogImport Job, last step, so the agent name is 
+associated with the proper extension.
+28800 seconds in an 8 hour day
+Run CallLogImport Job in SQL:
+EXEC MSDB.dbo.sp_start_job @Job_Name = 'CallLogImport'
+EXEC MSDB.dbo.sp_help_job @job_name = 'CallLogImport', @job_aspect = 'JOB'
+*/
+
+-- Purchasing -------------------------------------------------------------------------------------------------
+
+select c.calldate, 
+c.type as CallType, 
+datediff(second,'00:00:00',c.duration) as Duration,
+c.src as Extension
+
+from CDR_Report c
+where c.type = 'outbound'
+and c.src in ('1616','1359','1015','1076','1014','1360','1361','1104','1027')
+--and c.src = @Ext
+--and c.src in (select distinct Extension from acd_report)
+
+
+union all
+
+select c.calldate, 
+c.type as CallType, 
+datediff(second,'00:00:00',c.duration) as Duration,
+c.dst as Extension
+
+from CDR_Report c
+where c.type = 'inbound'
+and c.dst in ('1616','1359','1015','1076','1014','1360','1361','1104','1027')
+--and c.dst = @Ext
+--and c.dst in (select distinct Extension from acd_report)
+
+union all
+
+select c.calldate, 
+'Internal Out' as CallType, 
+datediff(second,'00:00:00',c.duration) as Duration,
+c.src as Extension
+
+from CDR_Report c
+
+where c.type = 'ext2ext' 
+and c.src in ('1616','1359','1015','1076','1014','1360','1361','1104','1027')
+and c.src <> c.dst
+--and c.src = @Ext
+--and c.src in (select distinct Extension from acd_report)
+
+union all
+
+select c.calldate, 
+'Internal In' as CallType, 
+datediff(second,'00:00:00',c.duration) as Duration,
+c.dst as Extension
+
+from CDR_Report c
+where c.type = 'ext2ext' 
+and c.dst in ('1616','1359','1015','1076','1014','1360','1361','1104','1027')
+and c.src <> c.dst
+--and c.dst = @Ext
+--and c.dst in (select distinct Extension from acd_report)
